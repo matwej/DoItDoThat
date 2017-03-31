b1cb5fd4e2889bcb04f856df266521798fc70d88
pivarci <pivarci.matej@gmail.com>
2017-03-31 16:36:12 +0200

    app controller locale and csrf ignore

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::API
   include ActionController::Serialization
+  protect_from_forgery with: :null_session
+  before_action :set_locale
+
+  protected
+
+  def set_locale
+    I18n.locale = request.headers['Accept-Language']
+  end
 end
